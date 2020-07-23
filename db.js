46e5dea709700ee2ff06677df3750dfc5ff7806e
alvislim <alvislws@gmail.com>
2020-07-23 19:38:20 +0800

    added env viarable

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = connectDB = async () => {
     try {
-        const mongoURI = "mongodb+srv://learning:hateherla@cluster0-dlmch.mongodb.net/reactp1?retryWrites=true&w=majority/animalshelter"
+        const mongoURI = process.env.MONGODB_URI
         const conn = await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useCreateIndex: true,
