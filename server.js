94617e7706e1510cf0d6ba235ebe2a8a5acb830b
alvislim <alvislws@gmail.com>
2020-07-23 20:20:26 +0800

    s

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded( {extended: true} ))
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://alvislim.github.io/animalshelter-client/'
+}))
 
 require('./db')
 connectDB()
