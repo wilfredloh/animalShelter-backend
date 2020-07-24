4d47a80bb057b24874868e7322db9786dbef30ee
wilfredloh <lohwilfred19@gmail.com>
2020-07-25 02:58:22 +0800

    cors

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded( {extended: true} ))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({}))
 
 require('./db')
 connectDB()
